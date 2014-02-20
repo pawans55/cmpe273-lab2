00d557ed78af9b82f62faf277a2456435facd4d1
Pawan Shrivastava <pawan.shrivastava@sjsu.edu>
2014-02-19 17:51:44 -0800

    Update login.js

--- login.js
+++ login.js
@@ -1,25 +1,21 @@
-
-/**
- * Login Class
- */
 function Login() {
-	// sessionId -> user map
-	this.sessionMap = {
-		99999 : { name: 'Foo', email: 'foo@bar.com' }
-	};
+        // sessionId -> user map
+        this.sessionMap = {
+                99999 : { name: 'Foo', email: 'foo@bar.com' }
+        };
 }
 /**
  * Say Hello {name} to the user
  */
 Login.prototype.hello = function(sessionId) {
-	return 'Hello ' + this.sessionMap[sessionId].name + '\n';
+        return 'Hello ' + this.sessionMap[sessionId].name + '\n';
 };
 
 /**
  * Check whether the given session id is valid (is in sessionMap) or not.
  */
 Login.prototype.isLoggedIn = function(sessionId) {
-	return sessionId in this.sessionMap;
+        return sessionId in this.sessionMap;
 };
 
 /**
@@ -27,24 +23,59 @@ Login.prototype.isLoggedIn = function(sessionId) {
  */
 Login.prototype.login = function(_name, _email) {
    /*
-	* Generate unique session id and set it into sessionMap like foo@bar.com
-	*/
-	var sessionId = new Date().getTime();
-	this.sessionMap[sessionId] = { name: _name, email: _email } 
-	
-	console.log('new session id ' + sessionId + ' for login::' + _email);
-	
-	return sessionId;
+        * Generate unique session id and set it into sessionMap like foo@bar.com
+        */
+        var sessionId = new Date().getTime();
+        this.sessionMap[sessionId] = { name: _name, email: _email }
+
+        console.log('new session id ' + sessionId + ' for login::' + _email);
+
+        return sessionId;
+};
+
+Login.prototype.refreshid = function(sessionId) {
+
+        var newsessionId = new Date().getTime();
+        var previous_name = this.sessionMap[sessionId].name;
+        var previous_email = this.sessionMap[sessionId].email;
+        
+        this.sessionMap[newsessionId]= { name: previous_name, email: previous_email};
+        delete this.sessionMap[sessionId];
+
+        //var newsessionId = new Date().getTime();
+        //this.sessionMap[newsessionId]= { name: old_name, email: old_email}
+        
+        //console.log('logout::' + sessionId);
+
+        //console.log('new session id '+ newsessionId + ' for login ::' + old_email);
+        return newsessionId;
+   /*
+        * TODO: Remove the given sessionId from the sessionMap
+        */
+};
+
+Login.prototype.delete = function(sessionId) {
+
+        delete this.sessionMap[sessionId];
+
+
+        console.log('logout::' + sessionId);
+   /*
+        * TODO: Remove the given sessionId from the sessionMap
+        */
 };
 
 /**
  * Logout from the server
- */ 
+ */
 Login.prototype.logout = function(sessionId) {
-	console.log('logout::' + sessionId);
+
+        delete this.sessionMap[sessionId];
+
+        console.log('logout::' + sessionId);
    /*
-	* TODO: Remove the given sessionId from the sessionMap
-	*/
+        * TODO: Remove the given sessionId from the sessionMap
+        */
 };
 
 // Export the Login class
